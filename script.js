a32c75dc476c8744dc68a27c18a411daceafc72e
Shubhangi Kumari Sharma <141427393+Shubhu2794@users.noreply.github.com>
2024-01-30 19:39:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -75,22 +75,21 @@ const checkWinner = () => {
     let drawFlag = true;
 
   for (let pattern of winPatterns) {
+  for (let pattern of winPatterns) {
     let pos1Val = boxes[pattern[0]].innerText;
     let pos2Val = boxes[pattern[1]].innerText;
     let pos3Val = boxes[pattern[2]].innerText;
 
-    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+    if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
       if (pos1Val === pos2Val && pos2Val === pos3Val) {
         showWinner(pos1Val);
         return; // If there's a winner, exit the function
       }
-    } else {
-      drawFlag = false;
     }
   }
 
-  // Check for a draw after iterating through all patterns
-  if (drawFlag && count === 9) {
+  // Check for a draw
+  if (count === 9) {
     disableBoxes();
     msg.innerText = "Game is DRAW!";
     msgContainer.classList.remove("hide");
